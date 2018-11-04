cb524643fe72465511fefba1cfd1d04f9cbd9d34
William Ritchie <will.ritchie@yahoo.com>
2018-11-03 17:46:35 -0700

    Gradle Project Build Files

---
+++ desktop/src/com/mygdx/simpledropgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.simpledropgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.simpledropgame.SimpleDropGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SimpleDropGame(), config);
+	}
+}
